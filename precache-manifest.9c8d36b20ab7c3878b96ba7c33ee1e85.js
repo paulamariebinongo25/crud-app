e3c4d6f0c35d3d29c486979015dedd2df97f8e28
paulangcrackers <bmariepaula121@gmail.com>
2020-02-16 00:57:15 +0800

    Updates

---
+++ precache-manifest.9c8d36b20ab7c3878b96ba7c33ee1e85.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d0e61e64a71e11ef50b6d596264811",
+    "url": "/crud-app/index.html"
+  },
+  {
+    "revision": "99b83d711cd6c635aea1",
+    "url": "/crud-app/static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "1cd3e8f5bef0c5be46f3",
+    "url": "/crud-app/static/css/main.81b24cb8.chunk.css"
+  },
+  {
+    "revision": "99b83d711cd6c635aea1",
+    "url": "/crud-app/static/js/2.1ea5eeaa.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/crud-app/static/js/2.1ea5eeaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd3e8f5bef0c5be46f3",
+    "url": "/crud-app/static/js/main.2ece2b2f.chunk.js"
+  },
+  {
+    "revision": "794ea53ace76b70639b1",
+    "url": "/crud-app/static/js/runtime-main.bba1dde0.js"
+  },
+  {
+    "revision": "2f3b4dc3e747c1e453c708b913ae7d06",
+    "url": "/crud-app/static/media/itunes.2f3b4dc3.png"
+  },
+  {
+    "revision": "c87b80397e325446608ac9a0a4cf311b",
+    "url": "/crud-app/static/media/myspace.c87b8039.png"
+  },
+  {
+    "revision": "d099141211c04d215cc3e45287be412b",
+    "url": "/crud-app/static/media/worldMap.d0991412.png"
+  }
+]);
